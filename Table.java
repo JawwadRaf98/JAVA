9e7b837575e943102e80f96e85a994f1f118e836
JawwadRaf98 <jawwad.rafique009@gmail.com>
2020-05-31 04:12:20 +0500

    Practice Till 3rd Chapter

---
+++ Table.java
@@ -0,0 +1,11 @@
+public class Table {
+    public static void main(String[] args) {
+        int  num =3;
+        int multiple = 10;
+        for(int i=1;i<=multiple;i++){
+            System.out.println(num+" X "+i+" = "+num*i);
+        }        
+        
+
+}
+}
